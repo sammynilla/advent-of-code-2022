0b1aa0c7c450ad25b07200356683946332a6a1fe
Samantha Bartlett <sammynilla@protonmail.com>
2022-12-01 13:50:01 -0900

    part 2 of day 1

--- day-1/app.c
+++ day-1/app.c
@@ -7,18 +7,32 @@ int main(int argc, char** argv) {
   char buf[1024];
   FILE* file = fopen(filename, "r");
   {
-    static long big, sum, temp;
-    char* endptr;
+    static long most_capacity[3];
+    static long sum, temp;
+    char* endptr = NULL;
     for (;;) {
       if (!fgets(buf, 1024, file)) {
-        printf("big: %ld\n", big);
+        int i;
+        sum = 0;
+        for (i = 0; i < 3; ++i) { sum += most_capacity[i]; }
+        printf("top capacities: %ld, %ld, %ld\n", most_capacity[0], most_capacity[1], most_capacity[2]);
+        printf("total calories: %ld\n", sum);
         break;
       }
 
       temp = strtol(buf, &endptr, 10);
 
       if (endptr == buf) {
-        big = sum > big ? sum: big;
+        int i;
+        /* printf("sum: %ld\n", sum); */
+        for (i = 0; i < 3; ++i) {
+          if (sum > most_capacity[i]) {
+            temp = most_capacity[i];
+            most_capacity[i] = sum;
+            most_capacity[i+1] = temp;
+            break;
+          }
+        }
         temp = sum = 0;
       }
       else {
