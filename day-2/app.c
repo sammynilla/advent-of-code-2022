4678dcc09eea50065c4731cb052d0d805f10bdc8
Samantha Bartlett <sammynilla@protonmail.com>
2022-12-02 13:49:40 -0900

    day 2 part 2

--- day-2/app.c
+++ day-2/app.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define elif else if
+
 static const char* filename = "data.txt";
 
 int main(int argc, char** argv) {
@@ -19,23 +21,20 @@ int main(int argc, char** argv) {
       b = (int)(buf[2]);
 
       switch (b) {
-        case X: score += 1; break;
-        case Y: score += 2; break;
-        case Z: score += 3; break;
-      }
-
-      switch (a) {
-        case A: {
-          if (b == X) score += 3;
-          else if (b == Y) score += 6;
+        case X: {
+          if (a==A)   score += 3;
+          elif (a==B) score += 1;
+          elif (a==C) score += 2;
         }break;
-        case B: {
-          if (b == Y) score += 3;
-          else if (b == Z) score += 6;
+        case Y: {
+          score += (a-A)+1;
+          score += 3;
         }break;
-        case C: {
-          if (b == Z) score += 3;
-          else if (b == X) score += 6;
+        case Z: {
+          if (a==A)   score += 2;
+          elif (a==B) score += 3;
+          elif (a==C) score += 1;
+          score += 6;
         }break;
       }
     }
